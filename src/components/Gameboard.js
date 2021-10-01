98ef403ac9ae15ca6c54e3890d1e09d7d07dff5f
abbasmurudkar <abbasmurudkar18@gmail.com>
2021-10-01 19:30:37 +0530

    Reactjs

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -8,6 +8,9 @@ function Gameboard() {
                 <Square value={0}/>
                 <Square value={1}/>
                 <Square value={2}/>
+                <Square>
+                    <p>hello guys can you play the game</p>
+                </Square>
             </div>
             <div className="rows">
                 <Square value={3}/>
