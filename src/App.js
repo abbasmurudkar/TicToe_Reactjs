31d4d63d094d131d32b4318814a4f61fbda7e27f
abbasmurudkar <abbasmurudkar18@gmail.com>
2021-10-02 10:02:53 +0530

    Reactjs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Gameboard from './components/Gameboard';
 import './STYLES/Global.scss'
 function App() {
+  
   return (
     <div className="container">
     <h1>TIC TOE GAME</h1>
-    <Gameboard/>
+    <Gameboard  />
+    {/* update={()=>{update()}} */}
     </div>
   );
 }
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 
 function Gameboard() {
+    const [gameboard, setgameboard] = useState(Array(9).fill(null))
+    const [zero, setzero] = useState(false)
+    console.log(gameboard)
+    const handleclick = (position) => {
+
+        if (gameboard[position]) {
+            return;
+        }
+
+        setgameboard((previous) => {
+            return previous.map((square, pos) => {
+                if (pos === position)   //map has its own index value so that it can compare index value with position of square
+                {
+                    return zero ? 'X' : '0'
+                }
+                return square    //returning the values
+            })
+        })
+        setzero((previous) => !previous) //this statement will make true for zero and then again false
+    }
+
+    const rendersquare = (position) => {
+        return <Square value={gameboard[position]} update={() => { handleclick(position) }} />  /* if we directly put the function while using props it will immediately execute for that use arrow function like this so that it works proper*/
+    }
     return (
         <div className="board">
             <div className="board-rows">
-                <Square value={0}/>
-                <Square value={1}/>
-                <Square value={2}/>
+                {rendersquare(0)}
+                {rendersquare(1)}
+                {rendersquare(2)}
             </div>
             <div className="board-rows">
-                <Square value={3}/>
-                <Square value={4}/>
-                <Square value={5}/>
+                {rendersquare(3)}
+                {rendersquare(4)}
+                {rendersquare(5)}
             </div>
             <div className="board-rows">
-                <Square value={6}/>
-                <Square value={7}/>
-                <Square value={8}/>
+                {rendersquare(6)}
+                {rendersquare(7)}
+                {rendersquare(8)}
             </div>
         </div>
     )
