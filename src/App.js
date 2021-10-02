122060e12d1d7abed701b1fe456cad7a74b7889b
abbasmurudkar <abbasmurudkar18@gmail.com>
2021-10-02 19:54:31 +0530

    upadted code

--- src/App.js
+++ src/App.js
@@ -4,34 +4,70 @@ import { useState } from 'react';
 import './STYLES/Global.scss'
 import { calculateWinner } from './helper';
 function App() {
-  const [gameboard, setgameboard] = useState(Array(9).fill(null))
-  const [zero, setzero] = useState(false)
-  const winner = calculateWinner(gameboard)
-  const message = winner ? `Winner is ${winner}`: `Next Player ${zero?'X':'0'}`
-console.log(winner)
 
-  console.log(gameboard)
+
+
+  // first use this two logic and then go ahead 
+  // const [gameboard, setgameboard] = useState(Array(9).fill(null))
+  // const [zero, setzero] = useState(false)  
+  // after using the first logic slightly change logic for history process
+
+
+
+  const [history, sethistory] = useState(
+    [
+      {  //creating array objects
+        gameboard: Array(9).fill(null),
+        zero: true
+      }
+    ]
+  )
+
+
+  const [currentmove, setcurrentmove] = useState(0)
+  const current = history[currentmove]   //state of the game
+  const winner = calculateWinner(current.gameboard)
+  const message = winner ? `Winner is ${winner}` : `Next Player turn ${current.zero ? 'X' : '0'}`
+  console.log(winner)
+  console.log(current.gameboard)
+
+
+
   const handleclick = (position) => {
 
-    if (gameboard[position] || winner) {  //if board position exist then return this code is written because it will dont allow to overwrite on it
+    if (current.gameboard[position] || winner) {  
+    //if board position exist then return this code is written because it will dont allow to overwrite on it
       return;
     }
-    setgameboard((previous) => {
-      return previous.map((square, pos) => {
-        if (pos === position)   //map has its own index value so that it can compare index value with position of square
+    // setgameboard((previous) => {  previously it was this 
+    sethistory((previous) => {
+      const last = previous[previous.length - 1] 
+      //it will give last element in this previous array
+
+
+
+      // return previous.map((square, pos) => {  //it was this before mapping the previous value
+      const newboard = last.gameboard.map((square, pos) => {
+        if (pos === position)   
+        //map has its own index value so that it can compare index value with position of square
         {
-          return zero ? 'X' : '0'
+          // return current.zero ? 'X' : '0' it is giving the x and 0 in the previous solution previously it was state 
+          return last.zero ? 'X' : '0'   
+          //but now it is state array object
         }
-        return square    //returning the values
+        return square    
+        //returning the values
       })
+      return previous.concat({gameboard: newboard, zero:!last.zero}) //changing the zero boolean value
     })
-    setzero((previous) => !previous) //this statement will make true for zero and then again false
+    // setzero((previous) => !previous) //this statement will make true for zero and then again false
+    setcurrentmove(previous=>previous+1)
   }
   return (
     <div className="container">
       <h1>TIC TOE GAME</h1>
       <h2>{message}</h2>
-      <Gameboard gameboard={gameboard} handleclick={handleclick} />
+      <Gameboard gameboard={current.gameboard} handleclick={handleclick} />
     </div>
   );
 }
